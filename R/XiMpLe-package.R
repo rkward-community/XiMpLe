e7055c6245b188d2b100ba28db2168d543ca0ea0
unDocUMeantIt <m@reaktanz.de>
2017-03-01 21:08:45 +0100

    rebuilt docs using devtools::document()
      - found a more robust workflow for roxyPackage::roxy.package() in conjunction with devtools::document()

--- R/XiMpLe-package.R
+++ R/XiMpLe-package.R
@@ -4,19 +4,19 @@
 #' Package: \tab XiMpLe\cr
 #' Type: \tab Package\cr
 #' Version: \tab 0.03-24\cr
-#' Date: \tab 2016-02-08\cr
+#' Date: \tab 2017-03-01\cr
 #' Depends: \tab R (>= 2.9.0),methods\cr
 #' Encoding: \tab UTF-8\cr
 #' License: \tab GPL (>= 3)\cr
 #' LazyLoad: \tab yes\cr
-#' URL: \tab http://reaktanz.de/?c=hacking&s=XiMpLe\cr
+#' URL: \tab https://reaktanz.de/?c=hacking&s=XiMpLe\cr
 #' }
 #'
-#' Provides a simple XML tree parser/generator. It includes functions to read XML files into R objects,
-#' get information out of and into nodes, and write R objects back to XML code.
-#' It's not as powerful as the 'XML' package and doesn't aim to be, but for simple XML handling
-#' it could be useful. It was originally developed for the R GUI and IDE RKWard (https://rkward.kde.org), to make plugin
-#' development easier.
+#' Provides a simple XML tree parser/generator. It includes functions to read XML files into
+#' R objects, get information out of and into nodes, and write R objects back to XML code.
+#' It's not as powerful as the 'XML' package and doesn't aim to be, but for simple XML
+#' handling it could be useful. It was originally developed for the R GUI and IDE RKWard
+#' (https://rkward.kde.org), to make plugin development easier.
 #'
 #' @aliases XiMpLe-package
 #' @name XiMpLe-package
