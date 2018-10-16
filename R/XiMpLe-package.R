e764fb43bee3b941bccd795b990a45a5c4dfe2ae
unDocUMeantIt <m@reaktanz.de>
2018-10-16 20:28:13 +0200

    trying to fix an issue with tidying, not yet successful

--- R/XiMpLe-package.R
+++ R/XiMpLe-package.R
@@ -5,7 +5,7 @@
 #' Package: \tab XiMpLe\cr
 #' Type: \tab Package\cr
 #' Version: \tab 0.10-3\cr
-#' Date: \tab 2018-05-21\cr
+#' Date: \tab 2018-10-15\cr
 #' Depends: \tab R (>= 2.9.0)\cr
 #' Encoding: \tab UTF-8\cr
 #' License: \tab GPL (>= 3)\cr
